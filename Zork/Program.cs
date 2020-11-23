6386dd185a5a5178eadc322c308081a9c82f9439
Nathan <nathanlawter@knights.ucf.edu>
2020-11-22 19:56:11 -0500

    Zork 2.2 & 2.3 - Expanding the Frontier

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -28,10 +28,10 @@ namespace Zork
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine("A rubber mat saying 'Weclome to Zork!' lies by the door.");
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
@@ -39,30 +39,33 @@ namespace Zork
                     case Commands.WEST:
                         if (Move(command) == false)
                         {
-
                             Console.WriteLine("The way is shut!");
                         }
                         break;
 
                     default:
-                        outputString = "Unknown command.";
+                        Console.WriteLine("Unknown command");
                         break;
                 }
-                Console.WriteLine(outputString);
             }
         }
 
         private static bool Move(Commands command)
         {
+            Assert.IsTrue(IsDirection(command), "Invalid direction.");
+
             bool isValidMove = true;
             switch (command)
             {
-                case Commands.NORTH:
-                case Commands.SOUTH:
-                    isValidMove = false;
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
                     break;
 
-                case Commands.EAST when Location.Column < Rooms.Length - 1:
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
+
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
                     Location.Column++;
                     break;
 
@@ -78,12 +81,22 @@ namespace Zork
         }
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
 
         private static readonly string[,] Rooms =
         {
-            {"Forest", "West of House", "Behind House", "Clearing", "Canyon View" }
+            {"Rocky Trail", "South of House", "Canyon View"},
+            {"Forest", "West of House", "Behind House" },
+            {"Dense Woods", "North of House", "Clearing" }
         };
 
-        private static (int Row, int Column) Location = (0, 1);
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+        private static (int Row, int Column) Location = (1, 1);
     }
 }
